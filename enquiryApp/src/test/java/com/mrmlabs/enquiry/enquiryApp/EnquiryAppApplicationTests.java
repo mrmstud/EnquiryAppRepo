3beee4d0136a1b17083ab36db7f7988c6949a2de
Mahesh <mrm.stud@gmail.com>
2021-01-17 16:12:39 -0600

    Initial Commit

---
+++ enquiryApp/src/test/java/com/mrmlabs/enquiry/enquiryApp/EnquiryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrmlabs.enquiry.enquiryApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnquiryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
